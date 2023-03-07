97773f6ca6a6f14da1e795e416f0b951807ce741
bucurum <bucurum18@posta.pau.edu.tr>
2023-03-07 17:21:19 +0300

    Visualising With Gizmos Added guard system to enemies Added suspicionBehaviour to enemies when enemy track the player and loose, the enemy behaive suspicious for a time, and return to the patrol point

--- Assets/[Game]/Scripts/Control/AIController.cs
+++ Assets/[Game]/Scripts/Control/AIController.cs
@@ -1,4 +1,6 @@
 using RPG.Combat;
+using RPG.Core;
+using RPG.Movement;
 using UnityEngine;
 
 namespace RPG.Control
@@ -6,14 +8,20 @@ namespace RPG.Control
     public class AIController : MonoBehaviour
     {
         [SerializeField] float chaseDistance = 5f;
+        [SerializeField] float suspicionTime = 2f;
         GameObject player;
         Fighter fighter;
         Health health;
+        Mover mover;
+        Vector3 guardPosition;
+        float timeSinceLastSawPlayer = Mathf.Infinity;
         void Start()
         {
             fighter = GetComponent<Fighter>();
             player = GameObject.FindWithTag("Player");
             health = GetComponent<Health>();
+            guardPosition = transform.position;
+            mover = GetComponent<Mover>();
         }
         void Update()
         {
@@ -23,19 +31,45 @@ namespace RPG.Control
             }
             if (InAttackRangeOfPlayer() && fighter.CanAttack(player))
             {
-                Debug.Log(gameObject.name + " is chasing");
-                fighter.Attack(player);
+                timeSinceLastSawPlayer = 0;
+                AttackBehaviour();
+            }
+            else if(timeSinceLastSawPlayer < suspicionTime)
+            {
+                SuspicionBehaviour();
             }
             else
             {
-                fighter.Cancel();
+                GuardBehaviour();
             }
+            timeSinceLastSawPlayer += Time.deltaTime;
+        }
+
+        private void GuardBehaviour()
+        {
+            mover.StartMoveAction(guardPosition);
+        }
+
+        private void SuspicionBehaviour()
+        {
+            GetComponent<ActionScheduler>().CancelCurrentAction();
+        }
+
+        private void AttackBehaviour()
+        {
+            Debug.Log(gameObject.name + " is chasing");
+            fighter.Attack(player);
         }
 
         private bool InAttackRangeOfPlayer()
         {
             float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
             return distanceToPlayer < chaseDistance;
+        }
+        private void OnDrawGizmosSelected() // Called by unity gizmos
+        {
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawWireSphere(transform.position, chaseDistance);
         }
     }
 
